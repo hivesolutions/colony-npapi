16f861bc157c47e94f069b4e897e92eb80a54e95
joamag <joamag@gmail.com>
2013-07-04 09:53:44 +0100

    incremented version number

--- src/colony_npapi/stdafx.h
+++ src/colony_npapi/stdafx.h
@@ -122,7 +122,7 @@ unsigned char *description_colony_npapi();
 
 #define NPCOLONY_NAME "Colony Gateway Plugin"
 #define NPCOLONY_DESCRIPTION "<a href=\"http://getcolony.com/\">Colony Gateway</a> plugin."
-#define NPCOLONY_VERSION "1.1.1"
+#define NPCOLONY_VERSION "1.1.2"
 #define NPCOLONY_MIME "application/x-colony-gateway:colony:gateway@getcolony.com";
 #define NPCOLONY_BINIE "binie"
 #define NPCOLONY_PDF "pdf"

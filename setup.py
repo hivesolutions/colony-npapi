1e5544257b669d7c646d76b44c292c96e23f1e09
joamag <joamag@hive.pt>
2016-02-11 13:34:43 +0000

    new license values

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@
 # -*- coding: utf-8 -*-
 
 # Hive Colony Framework
-# Copyright (C) 2008-2016 Hive Solutions Lda.
+# Copyright (c) 2008-2016 Hive Solutions Lda.
 #
-# This file is part of Hive Colony Framework.
+# This file is part of Hive Colony Framework
 #
 # Hive Colony Framework is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
 #
 # Hive Colony Framework is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
+# Apache License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with Hive Colony Framework. If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the Apache License along with
+# Hive Colony Framework If not, see <http://www.apache.org/licenses/>.
 
-__author__ = "João Magalhães joamag@hive.pt>"
+__author__ = "João Magalhães <joamag@hive.pt>"
 """ The author(s) of the module """
 
 __version__ = "1.0.0"
@@ -34,7 +34,7 @@ __date__ = "$LastChangedDate$"
 __copyright__ = "Copyright (c) 2008-2016 Hive Solutions Lda."
 """ The copyright for the module """
 
-__license__ = "GNU General Public License (GPL), Version 3"
+__license__ = "Apache License, Version 2.0"
 """ The license for the module """
 
 import os
@@ -84,7 +84,7 @@ setuptools.setup(
     author = "Hive Solutions Lda.",
     author_email = "development@hive.pt",
     description = "Colony Framework",
-    license = "GNU General Public License (GPL), Version 3",
+    license = "Apache License, Version 2.0",
     keywords = "colony npapi native",
     url = "http://colony_npapi.hive.pt",
     zip_safe = False,
@@ -92,7 +92,7 @@ setuptools.setup(
     classifiers = [
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
